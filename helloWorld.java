1c6b3a4fa15be17bee5c889797de295af2e935fa
dearbornchisale <dearbornchisale@gmail.com>
2026-03-16 15:07:42 +0200

    Added first Java hello wolrd program

--- helloWorld.java
+++ helloWorld.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class helloWorld {
 public static void main(String[] args) {
+    Scanner input = new Scanner (System.in);
     
 
     System.out.println("Hello World. Welcome to my first Java program");
