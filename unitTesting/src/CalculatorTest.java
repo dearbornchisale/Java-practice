65a5622a4bfa7aea1d672df5859bc60bc58b436e
dearbornchisale <dearbornchisale@gmail.com>
2026-05-15 15:40:10 +0200

    Added JUnit Testing example

---
+++ unitTesting/src/CalculatorTest.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    @Test
+    void testAdd() {
+        Calculator calc = new Calculator();
+
+        assertEquals(5, calc.add(2, 3));
+        assertEquals(0, calc.add(-1, 1));
+    }
+
+    @Test
+    void testSubtract() {
+        Calculator calc = new Calculator();
+
+        assertEquals(2, calc.subtract(5, 3));
+        assertEquals(-1, calc.subtract(2, 3));
+    }
+
+    @Test
+    void testMultiply() {
+        Calculator calc = new Calculator();
+
+        assertEquals(6, calc.multiply(2, 3));
+        assertEquals(0, calc.multiply(0, 10));
+    }
+}
